2ae24e3cae8d7a6b470ad73d0f1d80923127cdd4
Rohan Sinha <105815725+rohannstu@users.noreply.github.com>
2022-09-09 20:55:17 +0600

    Update and rename CP1.cpp to mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,4 +1,4 @@
-//10 s√-1m(W)l(2.71..) (11)(2.71..)(√-1)(nitrogen)(9.8)// 
+//all is well
 #include <bits/stdc++.h>
 using namespace std;
 #define LLI long long int
@@ -12,11 +12,14 @@ using namespace std;
 #define yes     cout<<"YES"<<nl; 
 #define no      cout<<"NO"<<nl; 
 #define f(i, f, l) for (LLI i = f; i < l; i++)
+#define input(a)   for(LLI i=0; i<n; i++) cin>>a[i]; 
+#define output(a)  for(LLI i=0; i<n; i++) cout<<a[i]<<" "; 
 LLI gcd(LLI a, LLI b){return __gcd(a, b);}
 LLI lcm(LLI a, LLI b) { return a * (b / __gcd(a, b)); }
 bool isprime(LLI n){if (n <= 1) return false;for (LLI i = 2; i < n; i++)
- if (n % i == 0)return false;
+if (n % i == 0)return false;
 return true;}
+bool perfectsqr(LLI n){ LLI v;   v=sqrt(n); if(v*v==n) { return true; } else { return false; }}
 bool decending(int a,int b){ return a>b; }
 LLI sqr_sum(LLI n) { return (n * (n + 1) * (2 * n + 1)) / 6; }
 const int N=1e5+10;
@@ -25,48 +28,29 @@ const int MAX=1e7+10;
 LLI mod(LLI x){return ((x%M + M)%M);}
 LLI add(LLI a, LLI b){return mod(mod(a)+mod(b));}
 LLI mul(LLI a, LLI b){ return mod(mod(a)*mod(b));}
+//bit manipulation 
+int countsetbits(LLI n){LLI cnt=0;  while(n){cnt+=(n&1);  n=n>>1;  }return cnt; }
+int pow(int a, int b){if(b==0) return 1;int ret = pow(a,b/2);ret *= ret; if(b&1) ret *= a; return ret; }
+#define debug(x) cout<<":["<<x<<"XE]"<<endl;
+#define debug2(x,y) cout<<":["<<x<<" "<<y<<"XE]"<<endl;
 /*----------------------------------------------------------------------*/
-
 void solve() 
 {
-    LLI n,m,p; cin>>n>>m; 
-    LLI  a[n+1];  map<LLI,LLI>mp; LLI sum=0; 
-    for(LLI i=1; i<=n; i++) { cin>>a[i];   sum+=a[i];   }
-    for(LLI i=1; i<=n; i++) {mp[i]=a[i];  } 
-    LLI cur=0;  
+   
 
-    while(m--){
-        LLI t,x,i;   
-        cin>>t; 
-        if(t==2){
-          cin>>x; 
-          sum=n*x; 
-          cur=x;  //1 2 3 4 5 
-          mp.clear();    
-        }
-        else{
-           cin>>i>>x; 
-           if(!mp[i]) mp[i]=cur; 
-           sum-=mp[i]; 
-           sum+=x; mp[i]=x; 
-         
-        }
-        cout<<sum<<nl; 
-    }
 }
 
-
-int32_t main()
+signed main()
 {
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0); 
     cin.tie(0);
-    cout.tie(0); 
-   // LLI tc; cin >> tc;  
-    // cin.ignore();
-   // for (LLI i = 1; i <= tc; i++)
-    //{
-         //cout << "Case #" << i << ": ";
-        //solve(); 
-    //}
- solve();
+    cout.tie(0);  
+    LLI tc; cin >> tc;  
+    cin.ignore();
+    for (LLI i = 1; i <= tc; i++)
+    {
+        //cout << "Case " << i << ": ";
+        solve(); 
+    }
+    //solve();
 }
