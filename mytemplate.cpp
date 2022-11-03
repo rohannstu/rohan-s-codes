3ff063cd49bfd5b898dc80943d2bf7d063fbbb05
Rohan Sinha <105815725+rohannstu@users.noreply.github.com>
2022-11-04 02:21:23 +0600

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,8 +1,8 @@
 //all is well
 #include <bits/stdc++.h>
 using namespace std;
-#define LLI long long int
-#define Ld long double
+#define ll long long int
+#define ld long double
 #define sz(x) ((int)x.size())
 #define all(a) (a).begin(), (a).end()
 #define pb(x) push_back(x)
@@ -11,25 +11,25 @@ using namespace std;
 #define vec(a) vector<LLI>a
 #define yes     cout<<"YES"<<nl; 
 #define no      cout<<"NO"<<nl; 
-#define f(i, f, l) for (LLI i = f; i < l; i++)
-#define input(a)   for(LLI i=0; i<n; i++) cin>>a[i]; 
-#define output(a)  for(LLI i=0; i<n; i++) cout<<a[i]<<" "; 
-LLI gcd(LLI a, LLI b){return __gcd(a, b);}
-LLI lcm(LLI a, LLI b) { return a * (b / __gcd(a, b)); }
-bool isprime(LLI n){if (n <= 1) return false;for (LLI i = 2; i < n; i++)
+#define f(i, f, l) for (ll i = f; i < l; i++)
+#define input(a)   for(ll i=0; i<n; i++) cin>>a[i]; 
+#define output(a)  for(ll i=0; i<n; i++) cout<<a[i]<<" "; 
+ll gcd(ll a, ll b){return __gcd(a, b);}
+ll lcm(ll a, ll b) { return a * (b / __gcd(a, b)); }
+bool isprime(ll n){if (n <= 1) return false;for (ll i = 2; i < n; i++)
 if (n % i == 0)return false;
 return true;}
-bool perfectsqr(LLI n){ LLI v;   v=sqrt(n); if(v*v==n) { return true; } else { return false; }}
+bool perfectsqr(ll n){ ll v;   v=sqrt(n); if(v*v==n) { return true; } else { return false; }}
 bool decending(int a,int b){ return a>b; }
-LLI sqr_sum(LLI n) { return (n * (n + 1) * (2 * n + 1)) / 6; }
+ll sqr_sum(ll n) { return (n * (n + 1) * (2 * n + 1)) / 6; }
 const int N=1e5+10;
 const int M=1e9+7; 
 const int MAX=1e7+10;  
-LLI mod(LLI x){return ((x%M + M)%M);}
-LLI add(LLI a, LLI b){return mod(mod(a)+mod(b));}
-LLI mul(LLI a, LLI b){ return mod(mod(a)*mod(b));}
+ll mod(ll x){return ((x%M + M)%M);}
+ll add(ll a, ll b){return mod(mod(a)+mod(b));}
+ll mul(ll a, ll b){ return mod(mod(a)*mod(b));}
 //bit manipulation 
-int countsetbits(LLI n){LLI cnt=0;  while(n){cnt+=(n&1);  n=n>>1;  }return cnt; }
+int countsetbits(ll n){ll cnt=0;  while(n){cnt+=(n&1);  n=n>>1;  }return cnt; }
 int getBit(int n,int pos){ return ((n&(1<<pos))!=0); }
 int setBit(int n,int pos){ return (n | (1<<pos)); }
 int clearBit(int n,int pos){ int mask=~(1<<pos); return (n&mask); }
@@ -40,6 +40,8 @@ int pow(int a, int b){if(b==0) return 1;int ret = pow(a,b/2);ret *= ret; if(b&1)
 /*----------------------------------------------------------------------*/
 void solve() 
 {
+    ll n; cin>>n; cout<<n<<endl; 
+        
 }
 
 
@@ -49,12 +51,12 @@ signed main()
     ios_base::sync_with_stdio(0); 
     cin.tie(0);
     cout.tie(0);  
-    // LLI tc; cin >> tc;  
-    // cin.ignore();
-    // for (LLI i = 1; i <= tc; i++)
-    // {
-    //     //cout << "Case " << i << ": ";
-    //     solve(); 
-    // }
-    solve();
+    ll tc; cin >> tc;  
+    cin.ignore();
+    for (ll i = 1; i <= tc; i++)
+    {
+        //cout << "Case " << i << ": ";
+        solve(); 
+    }
+    //solve();
 }
